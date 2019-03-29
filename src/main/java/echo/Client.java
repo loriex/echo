73bf8ae8538614205bc1f5c7059e13a93af3a0b9
Youwei.Li <youwei.li@hulu.com>
2019-03-30 00:11:40 +0800

    quickstart

---
+++ src/main/java/echo/Client.java
@@ -0,0 +1,13 @@
+package echo;
+
+/**
+ * Hello world!
+ *
+ */
+public class Client 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "I'm Client!" );
+    }
+}
---
+++ src/main/java/echo/Server.java
@@ -0,0 +1,13 @@
+package echo;
+
+/**
+ * Hello world!
+ *
+ */
+public class Server 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "I'm server!" );
+    }
+}
